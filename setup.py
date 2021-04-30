f4080f1b1e3c0145c0d7a557d6e4de75f01d49b6
﻿Adam Urish <adamurish@gmail.com>
2021-04-30 16:37:01 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,19 @@
-etdasf
+import setuptools
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+setuptools.setup(
+    name="mnist_example-nanoMFG", # Replace with your own username
+    version="0.0.1",
+    author="Example Author",
+    author_email="author@example.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages()
+    python_requires='>=3.0',
+    install_requires=[
+        'django',
+        'pandas'
+    ]
+)
